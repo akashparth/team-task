f0d352765185d43f707e7a371f0d63c8dfab2d08
akashparth <142104143+akashparth@users.noreply.github.com>
2026-05-24 13:28:18 +0530

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,8 +3,26 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true, minlength: 6 },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+
+  role: {
+    type: String,
+    enum: ["Admin", "Member"],
+    default: "Member"
+  }
+
 }, { timestamps: true });
 
 userSchema.pre('save', async function(next) {
